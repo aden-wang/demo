f07a52394bcb244ef7352f2b10b2addd91c60f74
aden <aden.wang@qq.com>
2019-10-14 01:47:14 +0800

    添加第二条数据，准备提交到git远程仓库

--- src/main/java/com/baidu/gitdemo/test/GitDemo.java
+++ src/main/java/com/baidu/gitdemo/test/GitDemo.java
@@ -7,5 +7,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("演示Git");
         System.out.println("添加一条数据");
+        System.out.println("添加第二条数据，准备提交到git远程仓库");
     }
 }
