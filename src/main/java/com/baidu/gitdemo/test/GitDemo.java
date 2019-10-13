ab1f167701d4b3a18962a6fb34280e95c510d331
aden <aden.wang@qq.com>
2019-10-14 02:06:06 +0800

    sourccetree又提交了第三条数据

--- src/main/java/com/baidu/gitdemo/test/GitDemo.java
+++ src/main/java/com/baidu/gitdemo/test/GitDemo.java
@@ -8,5 +8,7 @@ public class GitDemo {
         System.out.println("演示Git");
         System.out.println("添加一条数据");
         System.out.println("添加第二条数据，准备提交到git远程仓库");
+        System.out.println("sourccetree又提交了第三条数据");
+
     }
 }
