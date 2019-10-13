07c1edd74fb92b6e9faa837c6e32c0e58f546db7
aden <aden.wang@qq.com>
2019-10-14 01:16:44 +0800

    创建gitdemo项目，添加GitDemo类，第一次上传

---
+++ src/main/java/com/baidu/gitdemo/test/GitDemo.java
@@ -0,0 +1,10 @@
+package com.baidu.gitdemo.test;
+
+/**
+ * Created by Aden on 2019/10/14 1:00
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("演示Git");
+    }
+}
